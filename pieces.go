8ede14458297df29f82d7e7556fd373ef22dda6b
Jake <jake@proteatech.dev>
2024-11-18 12:18:50 -0800

    Add basic unicode pieces

---
+++ pieces.go
@@ -0,0 +1,24 @@
+package main
+
+type piece string
+
+const (
+	// White pieces
+	WHITE_PAWN   piece = "♙"
+	WHITE_KNIGHT piece = "♘"
+	WHITE_BISHOP piece = "♗"
+	WHITE_ROOK   piece = "♖"
+	WHITE_QUEEN  piece = "♕"
+	WHITE_KING   piece = "♔"
+)
+
+const (
+	// Black pieces
+	BLACK_PAWN   piece = "♟"
+	BLACK_KNIGHT piece = "♞"
+	BLACK_BISHOP piece = "♝"
+	BLACK_ROOK   piece = "♜"
+	BLACK_QUEEN  piece = "♛"
+	BLACK_KING   piece = "♚"
+)
+
